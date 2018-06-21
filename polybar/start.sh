3cf3db8c7218968df96480dd662649f8873006fc
Enrico "erolm_a" Trombetta <20476442+erolm-a@users.noreply.github.com>
2018-06-21 22:18:07 +0200

    a lot of cleanup, new i3-gaps theme, cava, ncmpcpp, polybar, wine, conky is deprecated

--- polybar/start.sh
+++ polybar/start.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-# polybar top &
-polybar bottom &
-polybar top &
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar white &
+#polybar bar2 &
+
+echo "Bars launched..."
+
 exit 0
